0a801d9ac7f3617fb97f2b4e77c5501457859835
Harmonpreet Singh <45717781+Harmonpreet012@users.noreply.github.com>
2021-02-20 07:17:06 +0000

    Add files via upload

--- chatroom-backend/helloflask.py
+++ chatroom-backend/helloflask.py
@@ -1,1 +1,15 @@
+from flask import Flask
+import myroutes
 
+app = Flask(__name__, static_url_path='/static')
+##adding routes
+
+app.add_url_rule('/', view_func = myroutes.home)
+app.add_url_rule('/createRoom/<room_id>', view_func = myroutes.createRoom)
+app.add_url_rule('/closeRoom/<room_id>', view_func=myroutes.closeRoom)
+app.add_url_rule('/getMessages/<room_id>', view_func=myroutes.getMessages)
+app.add_url_rule('/sendMessage/<room_id>/<message>', view_func=myroutes.sendMessage)
+
+#main
+if __name__ == '__main__':
+    app.run()
