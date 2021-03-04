93c9e7f486c0da9544d2e39d4938e94d6098dc2c
Harmonpreet Singh <45717781+Harmonpreet012@users.noreply.github.com>
2021-03-04 22:02:59 +0000

    Add files via upload

--- chatroom-backend/myroutes.py
+++ chatroom-backend/myroutes.py
@@ -1,12 +1,13 @@
 from flask import  request,  send_file, render_template, redirect, jsonify, url_for
 import random
 rooms = set()
-chats = set()
+chats = list()
 
 def home():
     return render_template("home.html")
 
 def createRoom(room_id):
+    print("hello ", room_id)
     if int(room_id) not in rooms:
     	rooms.add(int(room_id))
     response = {
@@ -21,23 +22,22 @@ def closeRoom(room_id):
         return jsonify({"status":"ok"})
     return jsonify({"status":"room not found"})
 		
-def sendMessage(room_id, message):
+def sendMessage(room_id, user_name, message):
     if int(room_id) not in rooms:
         return jsonify({"status":"room not found"})
 
     room_id = int(room_id)
-    chats.add((room_id, message))
+    chats.append((room_id, user_name, message))
     return jsonify({"status":"ok"})
 
 def getMessages(room_id):
-
     room_id = int(room_id)
     if room_id not in rooms:
         return jsonify({"status":"room-not-found"})
     response = list()
     for i in chats:
         if i[0] == room_id:
-            response.append(i[1])
+            response.append((i[1], i[2]))
 
     return jsonify(response)
 
